0b10e24c2a1a3807e7bbeb8475c4d51e44735e0a
L0WSUN <144795189+L0WSUN@users.noreply.github.com>
2023-09-26 14:43:26 +0300

    check range

---
+++ check range.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+	int a, b, c;
+	cout << "Введите цифру: ";
+	cin >> a;
+	cout << "Введите диапазон: ";
+	cin >> b >> c;
+	if (b > c) swap(b, c);
+
+	cout << ((a >= b && a <= c) ? "Число входит в диапазон": "ошибка");
+	
+	
+}

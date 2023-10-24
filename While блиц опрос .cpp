763724669715ed6c7b167acd1a4549b030728037
L0WSUN <144795189+L0WSUN@users.noreply.github.com>
2023-10-24 12:21:00 +0300

    While блиц опрос
    24/10 блиц опрос задачки сделал 3/4

---
+++ While блиц опрос .cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;                                                           
+//int main()
+//{
+//    setlocale(LC_ALL, "");
+//    int a;
+//    a = 1;
+//    while (a <= 10) {
+//        cout << a << "";
+//        a += 1;
+//    }
+//
+//}   
+
+int main() {
+	int a, b;
+	cout << "дите";
+	cin >> a >> b;
+	if (a > b) swap(a, b);
+	while (b >= a) {
+		cout << b << "";
+		b-=1;
+		
+	}
+
+
+}
+//
+//int main() {
+//	int a, b;
+//	b = 0;
+//	cout << "vvod";
+//	cin >> a;
+//	while (a != 0) {
+//		cin >> a;
+//		b+=a;
+//		if (a == 0) cout << b;
+//	}
+//	
+//
+//
+//
+//}
